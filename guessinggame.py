0a019f0432e951a803ebc15c83ad5e0f047ba89f
sundered-night <poudeldevashish001@gmail.com>
2023-06-06 17:38:20 +0545

    nice one

---
+++ guessinggame.py
@@ -0,0 +1,21 @@
+from random import randint
+
+r = randint(1, 100)
+z = int(input("ENter the number of tries you want"))
+count = 1
+
+while count <= z:
+    n = int(input("Enter a number: "))
+
+    if n == r:
+        print("Congrats, you won the lottery")
+        break
+    elif n > r:
+        print("The number is lower")
+    else:
+        print("The number is higher")
+
+    count += 1
+    
+    
+print("The lottery number is {}".format(r))
